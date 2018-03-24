69bd2709cc16795193f87e3d1490000bb75aebdf
YILANLU <luyilan2012@hotmail.com>
2018-03-24 20:12:43 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,14 @@ class User < ApplicationRecord
   def is_member_of?(group)
     participated_groups.include?(group)
   end
-  
+
+  def join!(group)
+    participated_groups << group
+  end
+  def quit!(group)
+    participated_groups.delete(group)
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :group_relationships
